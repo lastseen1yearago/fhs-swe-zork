7ea0b7980c82e2c9eb3f16b2fdb8ceac5724dc08
lastseen1yearago <133504648+lastseen1yearago@users.noreply.github.com>
2023-07-24 11:25:20 +0200

    Braindump und Konzepte (24.07.2023)

--- main_zork.c
+++ main_zork.c
@@ -8,8 +8,39 @@ int main() {
     //Intro:
     intro();
 
-    //Akt 1:
+    const char* story_pieces[] = {
+        "Go to cafeteria.",
+        "Go to engine-room.",
+        "Go to storage.",
+        "Go to administration.",
+        "Pick up ID",
+        "Check crewmates IDs",
+        "Wake up Aaron.",
+        "Wake up Sterling.",
+        "Wake up Laura.",
+        "Wake up Malory.",
+        "Wake up Randy.",
+        ""
+    }
 
+// Entscheidungsbaum
+    const int numNodes = ?????;
+    int choices[][2] = {
+        ???
+    };
+
+// "Gameplay", also solange Fragen bis ein Game Over Status erreicht wird
+    int currentNode = 0;
+    while (1) {
+        printf("%s\n", story[currentNode]);
+
+        if (choices[currentNode][0] == -1) {
+            break;
+        }
+
+        int choice = getUserChoice("Was willst du tun?", &story[choices[currentNode][0]], 2);
+        currentNode = choices[currentNode][choice - 1];
+    }
+
+    return 0;
 }
-
-// Entscheidungsbaum anlegen!
--- zork.c
+++ zork.c
@@ -13,14 +13,29 @@ void intro() {
     Your head hurts and you can barely see. You slowly open your capsules door to find that 5 other capsules are still closed -  the others inside still asleep. \n
     Your mind clears up a little and you decide to play it safe. \n
     'Before I try to wake up anyone else, I should find out who they are, and more importantly, who I am.' \n
-    You take a look at your wrist, carrying a small screen displaying a map of the spaceship you are currently traveling on - the Marksman. \n\n
+    You take a look at your wrist, carrying a small screen displaying a map of the spaceship you are currently traveling on - the Marksman. Right now you are in the Medbay-Room. \n\n
     Task: \n
     Find your and everyone elses ID cards."):
 }
 
-int getuserchoice(???????) {
+// int getuserchoice(???????) {
     //returncodes:
     // >=  1 ->   Option 1
     //     0 ->   Option 2
     // <= -1 ->   Option 3
+//}
+
+int getUserChoice(const char* prompt, const char* choices[], int numChoices) {
+    int choice;
+    do {
+        printf("%s\n", prompt);
+        for (int i = 0; i < numChoices; i++) {
+            printf("%d. %s\n", i + 1, choices[i]);
+        }
+        printf("Deine Wahl: ");
+        scanf("%d", &choice);
+        getchar();
+    } while (choice < 1 || choice > numChoices);
+
+    return choice;
 }
